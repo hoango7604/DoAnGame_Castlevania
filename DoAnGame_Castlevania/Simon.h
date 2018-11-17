796e0b5890525ca54012d8407ed0f79d3d017306
voquochuyhuy <voquochuy798@gmail.com>
2018-11-17 16:25:15 +0700

    update

--- DoAnGame_Castlevania/Simon.h
+++ DoAnGame_Castlevania/Simon.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "GameObject.h"
-
+#include "Whip.h"
 #include"define.h"
 
 
@@ -12,20 +12,25 @@ class Simon : public CGameObject
 	
 	
 public:
-	
+	Whip *whip;
 	Simon() : CGameObject()
 	{
 		level = SIMON_LEVEL_BIG;
-		untouchable = 0;		
+		untouchable = 0;
+		whip = new Whip();
 	}
 	 
 	
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *colliable_objects = NULL);
 	virtual void Render();
-	
+	void SetPosition(float x, float y) {
+		this->x = x;
+		this->y = y;
+		whip->SetPosition(x, y);
+	}
 	void SetState(int state);
 	void SetLevel(int l) { level = l; }
 	void StartUntouchable() { untouchable = 1; untouchable_start = GetTickCount(); }
-	void SetPosition(float x, float y) { this->x = x, this->y = y; }
+	
 	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
 };
