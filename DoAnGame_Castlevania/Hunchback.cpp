2b51ca8eb3fa7212e4a4455b680309bee9f25323
hoango7604 <30996727+hoango7604@users.noreply.github.com>
2018-12-21 10:57:29 +0700

    update bird + hunchback

--- DoAnGame_Castlevania/Hunchback.cpp
+++ DoAnGame_Castlevania/Hunchback.cpp
@@ -1,4 +1,35 @@
-#include "Hunchback.h"
+﻿#include "Hunchback.h"
+
+void Hunchback::CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LPCOLLISIONEVENT>& coEvents)
+{
+	for (UINT i = 0; i < coObjects->size(); i++)
+	{
+		// Simon se khong va cham voi nhung vat sau:
+		if (dynamic_cast<Ground *>(coObjects->at(i)))
+		{
+			Ground *ground = dynamic_cast<Ground *>(coObjects->at(i));
+			float gl, gr, gt, gb;
+			ground->GetBoundingBox(gl, gt, gr, gb);
+
+			if (x < gr && x + HUNCHBACK_BBOX_WIDTH > gl &&
+				y < gb && y + HUNCHBACK_BBOX_HEIGHT > gt &&
+				vy > 0)
+			{
+				// Xét va chạm cứng
+				SetState(HUNCHBACK_STATE_IDLE);
+				isStickToGround = true;
+				idleCast = GetTickCount();
+			}
+
+			LPCOLLISIONEVENT e = SweptAABBEx(coObjects->at(i));
+
+			if (e->t > 0 && e->t <= 1.0f && vy > 0)
+				coEvents.push_back(e);
+			else
+				delete e;
+		}
+	}
+}
 
 void Hunchback::SetState(int state)
 {
@@ -6,7 +37,21 @@ void Hunchback::SetState(int state)
 
 	switch (state)
 	{
+	case HUNCHBACK_STATE_IDLE:
+		isIdle = true;
+		vx = 0;
+		vy = 0;
+		break;
+	case HUNCHBACK_STATE_JUMP:
+		isIdle = false;
+		vy = -(rand() % 4 + 1) * 0.1f;
+		isStickToGround = false;
 
+		vx = (rand() % 4 + 1) * 0.1f;
+		if (nx < 0)
+			vx = -vx;
+
+		break;
 	}
 }
 
@@ -20,19 +65,102 @@ void Hunchback::GetBoundingBox(float & left, float & top, float & right, float &
 
 void Hunchback::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	Enemy::Update(dt, coObjects);
+	if (bird->isDropHunchBack)
+	{
+		isStickToBird = false;
+	}
 
+	if (isStickToBird)
+	{
+		x = bird->x + BIRD_BBOX_WIDTH / 2;
+		y = bird->y + BIRD_BBOX_HEIGHT;
+	}
+	else
+	{
+		Enemy::Update(dt, coObjects);
 
+		vector<LPCOLLISIONEVENT> coEvents;
+		vector<LPCOLLISIONEVENT> coEventsResult;
 
-	x += dx;
-	y += dy;
+		CalcPotentialCollisions(coObjects, coEvents);
+
+		if (isIdle)
+		{
+			if (GetTickCount() - idleCast > HUNCHBACK_IDLE_TIME)
+			{
+				SetState(HUNCHBACK_STATE_JUMP);
+			}
+		}
+
+		if (!isStickToGround)
+			vy += SIMON_GRAVITY * dt;
+
+		// No collision occured, proceed normally
+		if (coEvents.size() == 0)
+		{
+			x += dx;
+			y += dy;
+		}
+		else
+		{
+			float min_tx, min_ty, nx = 0, ny;
+
+			FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny);
+
+			x += min_tx * dx + nx * 0.4f;		// nx*0.4f : need to push out a bit to avoid overlapping next frame
+			y += min_ty * dy + ny * 0.4f;
+
+			for (int i = 0; i < coEventsResult.size(); i++)
+			{
+				LPCOLLISIONEVENT e = coEventsResult[i];
+
+				if (dynamic_cast<Ground *>(e->obj))
+				{
+					// Da cham dat
+					// Khong va cham theo phuong ngang
+					if (e->ny < 0 || e->nx != 0)
+					{
+						// Xét va chạm cứng
+						SetState(HUNCHBACK_STATE_IDLE);
+						isStickToGround = true;
+						idleCast = GetTickCount();
+					}
+				}
+			}
+		}
+
+		// clean up collision events
+		for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
+	}
 }
 
 void Hunchback::Render()
 {
 	int ani = -1;
 
-
+	switch (state)
+	{
+	case HUNCHBACK_STATE_IDLE:
+		if (nx > 0)
+		{
+			ani = HUNCHBACK_ANI_IDLE_RIGHT;
+		}
+		else
+		{
+			ani = HUNCHBACK_ANI_IDLE_LEFT;
+		}
+		break;
+	case HUNCHBACK_STATE_JUMP:
+		if (nx > 0)
+		{
+			ani = HUNCHBACK_ANI_JUMP_RIGHT;
+		}
+		else
+		{
+			ani = HUNCHBACK_ANI_JUMP_LEFT;
+		}
+		break;
+	}
 
 	animations[ani]->Render(x, y);
 
--- DoAnGame_Castlevania/Hunchback.h
+++ DoAnGame_Castlevania/Hunchback.h
@@ -1,14 +1,29 @@
 #pragma once
 #include "Enemy.h"
+#include "Bird.h"
 #include "Ground.h"
 
 class Hunchback : public Enemy
 {
 public:
+	Bird *bird;
+	bool isStickToBird;
+	bool isStickToGround;
+	bool isIdle;
+	DWORD idleCast;
 
+	Hunchback(Bird *bird) 
+	{
+		this->bird = bird;
+		isStickToBird = true;
+		
+		state = HUNCHBACK_STATE_JUMP;
+		nx = bird->nx;
+		x = bird->x;
+		y = bird->y;
+	}
 
-	Hunchback() { }
-
+	virtual void CalcPotentialCollisions(vector<LPGAMEOBJECT> *coObjects, vector<LPCOLLISIONEVENT> &coEvents);
 	virtual void SetState(int state);
 	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
